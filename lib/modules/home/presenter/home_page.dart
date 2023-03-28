b9f2ea929961a5cf79a3cbd15f79b4d7c7b2db5f
Malt <malthusim5@gmail.com>
2023-03-28 12:20:36 -0300

    youtube AppBar

--- lib/modules/home/presenter/home_page.dart
+++ lib/modules/home/presenter/home_page.dart
@@ -1,11 +1,34 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: SizedBox(
+          height: 25,
+          child: Image.asset('assets/images/youtube_logo.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          const Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.star),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
